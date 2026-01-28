96b47eb215947fa797cfc6d6667af58ebbff19a5
Ryan Bergman <rbergman80@stuy.edu>
2026-01-28 15:06:46 -0500

    Extra Commit for Intake Impl merge conflicts

--- src/main/java/com/stuypulse/robot/subsystems/intake/IntakeImpl.java
+++ src/main/java/com/stuypulse/robot/subsystems/intake/IntakeImpl.java
@@ -1,37 +1,32 @@
 package com.stuypulse.robot.subsystems.intake;
 
-<<<<<<< Updated upstream
-public class IntakeImpl {
-=======
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.stuypulse.robot.constants.Motors;
 import com.stuypulse.robot.constants.Ports;
 
 // .setControl (new Follower()); //because we have two motors on the intake
 // one motor needs to be inverted
 public class IntakeImpl extends Intake {
-    private final TalonFX intakeMotor1;
-    private final TalonFX intakeMotor2;
+    private final SparkFlex intakeLeaderMotor;
+    private final SparkFlex intakeFollowerMotor;
 
     public IntakeImpl() {
-        intakeMotor1 = new TalonFX(Ports.Intake.LEADER_INTAKE_NEO);
-        intakeMotor2 = new TalonFX(Ports.Intake.FOLLOWER_INTAKE_NEO); //TODO: fill in actual ports
+        intakeLeaderMotor = new SparkFlex(Ports.Intake.LEADER_INTAKE_NEO, MotorType.kBrushless);
+        intakeFollowerMotor = new SparkFlex(Ports.Intake.FOLLOWER_INTAKE_NEO, MotorType.kBrushless); //TODO: fill in actual ports
 
-        Motors.Intake.intakeMotor1.configure(intakeMotor1);
-        Motors.Intake.intakeMotor2.configure(intakeMotor2);
+        intakeLeaderMotor.configure(Motors.Intake.intakeLeaderMotor, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        intakeFollowerMotor.configure(Motors.Intake.intakeFollowerMotor, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
     }
 
     @Override
     public void periodic() {
-        intakeMotor2.setControl(new Follower(Ports.Intake.LEADER_INTAKE_KRAKEN, MotorAlignmentValue.Opposed)); //TODO: fill in later
+        // OLD KRAKEN CODE: intakeFollowerMotor.set(new Follower(Ports.Intake.LEADER_INTAKE_KRAKEN, MotorAlignmentValue.Opposed)); //TODO: fill in later
     
-        intakeMotor1.setControl(new VelocityVoltage(getState().getVoltage()));
-
+        intakeLeaderMotor.set(1); //if this doesn't work then follower didn't work in configs. Try without Follower
 
     }
->>>>>>> Stashed changes
     
 }
